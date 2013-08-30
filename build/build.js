0ace6913788f7880e112a2f32fe8d84c6b427c46
Matt Mueller <mattmuelle@gmail.com>
2013-08-30 16:49:19 -0700

    setup gh-pages

---
+++ build/build.js
@@ -0,0 +1,728 @@
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module.exports) {
+    module.exports = {};
+    module.client = module.component = true;
+    module.call(this, module.exports, require.relative(resolved), module);
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("component-event/index.js", Function("exports, require, module",
+"\n\
+/**\n\
+ * Bind `el` event `type` to `fn`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} type\n\
+ * @param {Function} fn\n\
+ * @param {Boolean} capture\n\
+ * @return {Function}\n\
+ * @api public\n\
+ */\n\
+\n\
+exports.bind = function(el, type, fn, capture){\n\
+  if (el.addEventListener) {\n\
+    el.addEventListener(type, fn, capture || false);\n\
+  } else {\n\
+    el.attachEvent('on' + type, fn);\n\
+  }\n\
+  return fn;\n\
+};\n\
+\n\
+/**\n\
+ * Unbind `el` event `type`'s callback `fn`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} type\n\
+ * @param {Function} fn\n\
+ * @param {Boolean} capture\n\
+ * @return {Function}\n\
+ * @api public\n\
+ */\n\
+\n\
+exports.unbind = function(el, type, fn, capture){\n\
+  if (el.removeEventListener) {\n\
+    el.removeEventListener(type, fn, capture || false);\n\
+  } else {\n\
+    el.detachEvent('on' + type, fn);\n\
+  }\n\
+  return fn;\n\
+};\n\
+//@ sourceURL=component-event/index.js"
+));
+require.register("component-query/index.js", Function("exports, require, module",
+"\n\
+function one(selector, el) {\n\
+  return el.querySelector(selector);\n\
+}\n\
+\n\
+exports = module.exports = function(selector, el){\n\
+  el = el || document;\n\
+  return one(selector, el);\n\
+};\n\
+\n\
+exports.all = function(selector, el){\n\
+  el = el || document;\n\
+  return el.querySelectorAll(selector);\n\
+};\n\
+\n\
+exports.engine = function(obj){\n\
+  if (!obj.one) throw new Error('.one callback required');\n\
+  if (!obj.all) throw new Error('.all callback required');\n\
+  one = obj.one;\n\
+  exports.all = obj.all;\n\
+};\n\
+//@ sourceURL=component-query/index.js"
+));
+require.register("component-matches-selector/index.js", Function("exports, require, module",
+"/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var query = require('query');\n\
+\n\
+/**\n\
+ * Element prototype.\n\
+ */\n\
+\n\
+var proto = Element.prototype;\n\
+\n\
+/**\n\
+ * Vendor function.\n\
+ */\n\
+\n\
+var vendor = proto.matchesSelector\n\
+  || proto.webkitMatchesSelector\n\
+  || proto.mozMatchesSelector\n\
+  || proto.msMatchesSelector\n\
+  || proto.oMatchesSelector;\n\
+\n\
+/**\n\
+ * Expose `match()`.\n\
+ */\n\
+\n\
+module.exports = match;\n\
+\n\
+/**\n\
+ * Match `el` to `selector`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} selector\n\
+ * @return {Boolean}\n\
+ * @api public\n\
+ */\n\
+\n\
+function match(el, selector) {\n\
+  if (vendor) return vendor.call(el, selector);\n\
+  var nodes = query.all(selector, el.parentNode);\n\
+  for (var i = 0; i < nodes.length; ++i) {\n\
+    if (nodes[i] == el) return true;\n\
+  }\n\
+  return false;\n\
+}\n\
+//@ sourceURL=component-matches-selector/index.js"
+));
+require.register("component-delegate/index.js", Function("exports, require, module",
+"\n\
+/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var matches = require('matches-selector')\n\
+  , event = require('event');\n\
+\n\
+/**\n\
+ * Delegate event `type` to `selector`\n\
+ * and invoke `fn(e)`. A callback function\n\
+ * is returned which may be passed to `.unbind()`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} selector\n\
+ * @param {String} type\n\
+ * @param {Function} fn\n\
+ * @param {Boolean} capture\n\
+ * @return {Function}\n\
+ * @api public\n\
+ */\n\
+\n\
+exports.bind = function(el, selector, type, fn, capture){\n\
+  return event.bind(el, type, function(e){\n\
+    if (matches(e.target, selector)) fn(e);\n\
+  }, capture);\n\
+  return callback;\n\
+};\n\
+\n\
+/**\n\
+ * Unbind event `type`'s callback `fn`.\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {String} type\n\
+ * @param {Function} fn\n\
+ * @param {Boolean} capture\n\
+ * @api public\n\
+ */\n\
+\n\
+exports.unbind = function(el, type, fn, capture){\n\
+  event.unbind(el, type, fn, capture);\n\
+};\n\
+//@ sourceURL=component-delegate/index.js"
+));
+require.register("component-events/index.js", Function("exports, require, module",
+"\n\
+/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var events = require('event');\n\
+var delegate = require('delegate');\n\
+\n\
+/**\n\
+ * Expose `Events`.\n\
+ */\n\
+\n\
+module.exports = Events;\n\
+\n\
+/**\n\
+ * Initialize an `Events` with the given\n\
+ * `el` object which events will be bound to,\n\
+ * and the `obj` which will receive method calls.\n\
+ *\n\
+ * @param {Object} el\n\
+ * @param {Object} obj\n\
+ * @api public\n\
+ */\n\
+\n\
+function Events(el, obj) {\n\
+  if (!(this instanceof Events)) return new Events(el, obj);\n\
+  if (!el) throw new Error('element required');\n\
+  if (!obj) throw new Error('object required');\n\
+  this.el = el;\n\
+  this.obj = obj;\n\
+  this._events = {};\n\
+}\n\
+\n\
+/**\n\
+ * Subscription helper.\n\
+ */\n\
+\n\
+Events.prototype.sub = function(event, method, cb){\n\
+  this._events[event] = this._events[event] || {};\n\
+  this._events[event][method] = cb;\n\
+};\n\
+\n\
+/**\n\
+ * Bind to `event` with optional `method` name.\n\
+ * When `method` is undefined it becomes `event`\n\
+ * with the \"on\" prefix.\n\
+ *\n\
+ * Examples:\n\
+ *\n\
+ *  Direct event handling:\n\
+ *\n\
+ *    events.bind('click') // implies \"onclick\"\n\
+ *    events.bind('click', 'remove')\n\
+ *    events.bind('click', 'sort', 'asc')\n\
+ *\n\
+ *  Delegated event handling:\n\
+ *\n\
+ *    events.bind('click li > a')\n\
+ *    events.bind('click li > a', 'remove')\n\
+ *    events.bind('click a.sort-ascending', 'sort', 'asc')\n\
+ *    events.bind('click a.sort-descending', 'sort', 'desc')\n\
+ *\n\
+ * @param {String} event\n\
+ * @param {String|function} [method]\n\
+ * @return {Function} callback\n\
+ * @api public\n\
+ */\n\
+\n\
+Events.prototype.bind = function(event, method){\n\
+  var e = parse(event);\n\
+  var el = this.el;\n\
+  var obj = this.obj;\n\
+  var name = e.name;\n\
+  var method = method || 'on' + name;\n\
+  var args = [].slice.call(arguments, 2);\n\
+\n\
+  // callback\n\
+  function cb(){\n\
+    var a = [].slice.call(arguments).concat(args);\n\
+    obj[method].apply(obj, a);\n\
+  }\n\
+\n\
+  // bind\n\
+  if (e.selector) {\n\
+    cb = delegate.bind(el, e.selector, name, cb);\n\
+  } else {\n\
+    events.bind(el, name, cb);\n\
+  }\n\
+\n\
+  // subscription for unbinding\n\
+  this.sub(name, method, cb);\n\
+\n\
+  return cb;\n\
+};\n\
+\n\
+/**\n\
+ * Unbind a single binding, all bindings for `event`,\n\
+ * or all bindings within the manager.\n\
+ *\n\
+ * Examples:\n\
+ *\n\
+ *  Unbind direct handlers:\n\
+ *\n\
+ *     events.unbind('click', 'remove')\n\
+ *     events.unbind('click')\n\
+ *     events.unbind()\n\
+ *\n\
+ * Unbind delegate handlers:\n\
+ *\n\
+ *     events.unbind('click', 'remove')\n\
+ *     events.unbind('click')\n\
+ *     events.unbind()\n\
+ *\n\
+ * @param {String|Function} [event]\n\
+ * @param {String|Function} [method]\n\
+ * @api public\n\
+ */\n\
+\n\
+Events.prototype.unbind = function(event, method){\n\
+  if (0 == arguments.length) return this.unbindAll();\n\
+  if (1 == arguments.length) return this.unbindAllOf(event);\n\
+\n\
+  // no bindings for this event\n\
+  var bindings = this._events[event];\n\
+  if (!bindings) return;\n\
+\n\
+  // no bindings for this method\n\
+  var cb = bindings[method];\n\
+  if (!cb) return;\n\
+\n\
+  events.unbind(this.el, event, cb);\n\
+};\n\
+\n\
+/**\n\
+ * Unbind all events.\n\
+ *\n\
+ * @api private\n\
+ */\n\
+\n\
+Events.prototype.unbindAll = function(){\n\
+  for (var event in this._events) {\n\
+    this.unbindAllOf(event);\n\
+  }\n\
+};\n\
+\n\
+/**\n\
+ * Unbind all events for `event`.\n\
+ *\n\
+ * @param {String} event\n\
+ * @api private\n\
+ */\n\
+\n\
+Events.prototype.unbindAllOf = function(event){\n\
+  var bindings = this._events[event];\n\
+  if (!bindings) return;\n\
+\n\
+  for (var method in bindings) {\n\
+    this.unbind(event, method);\n\
+  }\n\
+};\n\
+\n\
+/**\n\
+ * Parse `event`.\n\
+ *\n\
+ * @param {String} event\n\
+ * @return {Object}\n\
+ * @api private\n\
+ */\n\
+\n\
+function parse(event) {\n\
+  var parts = event.split(/ +/);\n\
+  return {\n\
+    name: parts.shift(),\n\
+    selector: parts.join(' ')\n\
+  }\n\
+}\n\
+//@ sourceURL=component-events/index.js"
+));
+require.register("pinch/index.js", Function("exports, require, module",
+"/*\n\
+ * Module dependencies\n\
+ */\n\
+\n\
+var events = require('events');\n\
+\n\
+/**\n\
+ * Export `Pinch`\n\
+ */\n\
+\n\
+module.exports = Pinch;\n\
+\n\
+/**\n\
+ * Initialize `Pinch`\n\
+ *\n\
+ * @param {Element} el\n\
+ * @param {Function} fn\n\
+ * @return {Pinch}\n\
+ * @api public\n\
+ */\n\
+\n\
+function Pinch(el, fn) {\n\
+  if (!(this instanceof Pinch)) return new Pinch(el, fn);\n\
+  this.el = el;\n\
+  this.parent = el.parentNode;\n\
+  this.fn = fn || function(){};\n\
+  this.midpoint = null;\n\
+  this.events = events(el, this);\n\
+  this.events.bind('touchstart');\n\
+  this.events.bind('touchmove');\n\
+  this.events.bind('touchend');\n\
+  this.fingers = {};\n\
+}\n\
+\n\
+/**\n\
+ * Touch start\n\
+ *\n\
+ * @param {Event} e\n\
+ * @return {Pinch}\n\
+ * @api private\n\
+ */\n\
+\n\
+Pinch.prototype.ontouchstart = function(e) {\n\
+  e.preventDefault();\n\
+  this.scale = 1;\n\
+  var touches = e.touches;\n\
+  if (!touches.length) return this;\n\
+\n\
+  var changed = e.changedTouches;\n\
+  for (var i = 0, len = changed.length; i < len; i++) {\n\
+    this.fingers[changed[i].identifier] = {\n\
+      x: changed[i].pageX,\n\
+      y: changed[i].pageY\n\
+    };\n\
+  }\n\
+\n\
+  if (touches.length == 2) {\n\
+    var coords = [];\n\
+\n\
+    for(var id in this.fingers) {\n\
+      var finger = this.fingers[id];\n\
+      coords.push(finger.x);\n\
+      coords.push(finger.y);\n\
+    }\n\
+\n\
+    this.distance = distance.apply(null, coords);\n\
+    this.midpoint = midpoint.apply(null, coords);\n\
+  }\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Touch move\n\
+ *\n\
+ * @param {Event} e\n\
+ * @return {Pinch}\n\
+ * @api private\n\
+ */\n\
+\n\
+Pinch.prototype.ontouchmove = function(e) {\n\
+  var touches = e.touches;\n\
+  if (touches.length != 2) return this;\n\
+  var changed = e.changedTouches;\n\
+\n\
+  for (var i = 0, len = changed.length; i < len; i++) {\n\
+    var finger = this.fingers[changed[i].identifier];\n\
+    if (undefined === finger) continue;\n\
+    this.fingers[changed[i].identifier] = {\n\
+      x: changed[i].pageX,\n\
+      y: changed[i].pageY\n\
+    };\n\
+  }\n\
+\n\
+  var coords = [];\n\
+  for(var id in this.fingers) {\n\
+    var finger = this.fingers[id];\n\
+    coords.push(finger.x);\n\
+    coords.push(finger.y);\n\
+  }\n\
+\n\
+  var dist = distance.apply(null, coords);\n\
+  var mid = midpoint.apply(null, coords);\n\
+\n\
+  // iphone does scale natively, just use that\n\
+  e.scale = e.scale ? e.scale : dist / this.distance * this.scale;\n\
+  e.x = mid.x;\n\
+  e.y = mid.y;\n\
+\n\
+  this.fn(e);\n\
+\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Touchend\n\
+ *\n\
+ * @param {Event} e\n\
+ * @return {Pinch}\n\
+ * @api private\n\
+ */\n\
+\n\
+Pinch.prototype.ontouchend = function(e) {\n\
+  var changed = e.changedTouches;\n\
+  for (var i = 0, len = changed.length; i < len; i++) {\n\
+    delete this.fingers[changed[i].identifier];\n\
+  }\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Unbind\n\
+ *\n\
+ * @return {Pinch}\n\
+ * @api public\n\
+ */\n\
+\n\
+Pinch.prototype.unbind = function() {\n\
+  this.events.unbind();\n\
+  return this;\n\
+};\n\
+\n\
+\n\
+/**\n\
+ * Get the distance between two points\n\
+ *\n\
+ * @param {Number} x1\n\
+ * @param {Number} y1\n\
+ * @param {Number} x2\n\
+ * @param {Number} y2\n\
+ * @return {Number}\n\
+ * @api private\n\
+ */\n\
+\n\
+function distance(x1, y1, x2, y2) {\n\
+  var x = Math.pow(x1 - x2, 2);\n\
+  var y = Math.pow(y1 - y2, 2);\n\
+  return Math.sqrt(x + y);\n\
+}\n\
+\n\
+/**\n\
+ * Get the midpoint\n\
+ *\n\
+ * @param {Number} x1\n\
+ * @param {Number} y1\n\
+ * @param {Number} x2\n\
+ * @param {Number} y2\n\
+ * @return {Object} coords\n\
+ * @api private\n\
+ */\n\
+\n\
+function midpoint(x1, y1, x2, y2) {\n\
+  var coords = {};\n\
+  coords.x = (x1 + x2) / 2;\n\
+  coords.y = (y1 + y2) / 2;\n\
+  return coords;\n\
+}\n\
+//@ sourceURL=pinch/index.js"
+));
+
+
+require.alias("component-events/index.js", "pinch/deps/events/index.js");
+require.alias("component-events/index.js", "events/index.js");
+require.alias("component-event/index.js", "component-events/deps/event/index.js");
+
+require.alias("component-delegate/index.js", "component-events/deps/delegate/index.js");
+require.alias("component-matches-selector/index.js", "component-delegate/deps/matches-selector/index.js");
+require.alias("component-query/index.js", "component-matches-selector/deps/query/index.js");
+
+require.alias("component-event/index.js", "component-delegate/deps/event/index.js");
+
+require.alias("pinch/index.js", "pinch/index.js");
